a34006b4429d2a907b9d68fd415275bdebd79f05
Cian Dowling <cian.dowling@mycit.ie>
2017-12-12 03:41:00 +0000

    Updated DB with more Packages

--- Phillies-Flowers/src/main/resources/data.sql
+++ Phillies-Flowers/src/main/resources/data.sql
@@ -6,4 +6,8 @@ INSERT INTO user(UserName, UserPassword) VALUES('ciandow', 'cdpass');
 INSERT INTO package(PackageName, Description, PackageAmount) 
 VALUES 
 ('Easter Package', 'Some flowers and eggs and a rabbit', '25'),
-('Graduation Package', 'This will not be needed', '50');
+('Graduation Package', 'This will not be needed', '50'),
+('Christmas Package', 'A selection of MincePie scented Roses', '80'),
+('Weightloss Package', 'Medley of Non edidible highly toxic Daffodills', '30'),
+('Patriot Package', 'Red, White and Blue Roses', '40'),
+('Irish Package', 'Green, White and Orange Lillys', '20');
